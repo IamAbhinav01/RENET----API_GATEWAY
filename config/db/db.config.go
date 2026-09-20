db36e8c3350cde463e031162d3d3e288e661e2f7
Abhinav <abhinavsunil@hotmail.com>
2026-09-20 16:37:08 +0530

    Feat : connected the db with gorm

--- config/db/db.config.go
+++ config/db/db.config.go
@@ -1,28 +1,57 @@
 package db
 
-import "renet/config/env"
+import (
+	"fmt"
+	"log"
+	"renet/config/env"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 type DBConfig struct {
 	DBUSER string
 	DBPASS string
 	DBHOST string
-	DBPORT int
+	DBPORT string
 	DBName string
 	DBSSL  string
 }
 
 // dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 // db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-func InitDB() {
+func InitDB() (db *gorm.DB, err error){
 
 	cfg := DBConfig{
 		DBUSER: env.GetString("DBUSER"),
 		DBPASS: env.GetString("DBPASS"),
 		DBHOST: env.GetString("DBHOST"),
-		DBPORT: env.GetInt("DBPORT"),
+		DBPORT: env.GetString("DBPORT"),
 		DBName: env.GetString("DBName"),
 		DBSSL: env.GetString("DBSSL"),
 	}
+
+	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",cfg.DBHOST,cfg.DBUSER,cfg.DBPASS,cfg.DBName,cfg.DBPORT,cfg.DBSSL)
+
+	DB,DBerr := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+
+	if DBerr != nil{
+		return nil,fmt.Errorf("failed to connect to database: %w",DBerr)
+	}
 	
+	pgsql,err := DB.DB()
+	if err!=nil{
+		return nil,err
+	}
+
+	err = pgsql.Ping()
+	if err != nil{
+		log.Println("Failed to connected with the DB")
+		return nil,err
+	}
+	log.Println("Successfully connected with the DB")
+
+	return DB,nil
 
 }
