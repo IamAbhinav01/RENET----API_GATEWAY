4739bb23d5b5b3f3a4eb43e11d174f1f37d0b0f5
Abhinav <abhinavsunil@hotmail.com>
2026-09-21 12:38:58 +0530

    Feat : Added middleare for first entry gate of the application

--- middlewares/request.validator.go
+++ middlewares/request.validator.go
@@ -1,11 +1,14 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
 	dtos "renet/DTOs"
 	"renet/utils/converter"
 	"renet/utils/formatters"
 )
+
+var payloadContextKet string = "payload"
 
 func SignUpRequestValidation(next http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,11 @@ func SignUpRequestValidation(next http.Handler) http.Handler{
 			formatters.ErrorResponse(w,http.StatusBadRequest,err,"Error occured while decoding the json")
 			return 
 		}
+
+		reqContext := r.Context()
+		ctx := context.WithValue(reqContext,payloadContextKet,payload)
+
+		next.ServeHTTP(w,r.WithContext(ctx))
 
 	})
 }
