c86a85336767f2d2e895c99b1c252004818ad32f
Abhinav <abhinavsunil@hotmail.com>
2026-09-23 21:47:50 +0530

    Feat : added validators on middleware

--- middlewares/request.validator.go
+++ middlewares/request.validator.go
@@ -6,6 +6,7 @@ import (
 	dtos "renet/DTOs"
 	"renet/utils/converter"
 	"renet/utils/formatters"
+	"renet/utils/validators"
 )
 
 var PayloadContextKet string = "payload"
@@ -17,6 +18,13 @@ func SignUpRequestValidation(next http.Handler) http.Handler{
 		if err != nil{
 			formatters.ErrorResponse(w,http.StatusBadRequest,err,"Error occured while decoding the json")
 			return 
+		}
+
+		validationErr := validators.Validate.Struct(payload)
+
+		if validationErr != nil {
+				formatters.ErrorResponse(w,http.StatusBadRequest,validationErr,"Invalid Request Payload")
+				return 
 		}
 
 		reqContext := r.Context()
